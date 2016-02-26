9bd7c7df1837bc462afac50960b212a2c59ea3c6
Madz <dothide@gmail.com>
2016-02-26 15:45:28 +0800

    Adding semantically-release package

---
+++ src/index.test.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+var starWars = require('./index.js');
+
+describe('starwars-names', function() {
+  describe('all', function () {
+    it('should be an array of strings', function () {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string';
+        })
+      }
+    });
+
+    it('should contain `DotHide`', function () {
+      expect(starWars.all).to.include('DotHide');
+    });
+  });
+
+  describe('random', function () {
+    it('should return a random item from the starWars.all', function () {
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
+  });
+});
